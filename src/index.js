88de3bc499869f720c129649dc281eb3430fb86a
Ahmad Abugosh <ahmadabugosh@gmail.com>
2018-02-09 22:44:23 +0400

    Started project

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app =express();
+const React = require('react');
+
+const renderToString= require('react-dom/server').renderToString;
+const Home = require('./client/components/Home').default;
+
+app.get('/', (req,res)=> {
+
+	const content = renderToString(<Home />);
+	res.send(content);
+
+});
+
+app.listed(3000, ()=> {
+
+console.log('listening on port 3000');
+
+});
